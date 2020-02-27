f8cb92a002008b0be3eec6edf902f256102faa59
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-02-27 17:06:57 +0300

    Add initial Linked List implementation

--- data-structures/linked-list/LinkedList.ts
+++ data-structures/linked-list/LinkedList.ts
@@ -1,6 +1,31 @@
-import { LinkedListNode } from "./LinkedListNode";
+import { LinkedListNode, LinkedListNodeValue } from "./LinkedListNode";
 
 export function LinkedList() {
   this.head = null;
   this.tail = null;
+
+  this.prepend = function(value: LinkedListNodeValue) {
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+
+    if (!this.tail) this.tail = newNode;
+
+    return this;
+  };
+
+  this.append = function(value: LinkedListNodeValue) {
+    const newNode = new LinkedListNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    return this;
+  };
 }
